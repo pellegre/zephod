d1a3166c2133387ed849e8a19348f45315bb4b76
Esteban Pellegrino <pellegre.esteban@gmail.com>
2020-09-11 12:22:18 -0300

    automata definition

--- fa.py
+++ fa.py
@@ -1,16 +1,26 @@
 from pyauto.fsm import *
+import random
+import string
+
+
+def get_tmp_filename():
+    return "/tmp/" + "".join(random.choice(string.ascii_letters) for _ in range(12)) + ".pdf"
+
 
 if __name__ == '__main__':
     print("[+] FD ")
-    transition = Transition()
 
-    transition.add("e0", "e1", {0})
-    transition.add("e0", "e2", {1})
-    transition.add("e1", "e2", {1})
-    transition.add("e2", "e1", {0})
-    transition.add("e1", "e3", {0})
-    transition.add("e2", "e3", {1})
-    transition.add("e3", "e3", {0, 1})
+    transition = Transition()
+    transition.add("e0", "e1", {"0"})
+    transition.add("e0", "e2", {"1"})
+    transition.add("e1", "e2", {"1"})
+    transition.add("e2", "e1", {"0"})
+    transition.add("e1", "e3", {"0"})
+    transition.add("e2", "e3", {"1"})
+    transition.add("e3", "e3", {"0", "1"})
 
     fsm = FiniteAutomata(transition, "e0", {"e3"})
     print(fsm)
+
+    dot = fsm.build_dot()
+    dot.view(filename=get_tmp_filename())
--- pyauto/fsm.py
+++ pyauto/fsm.py
@@ -1,3 +1,22 @@
+from graphviz import Digraph
+
+
+class State:
+    def __init__(self, name):
+        self.name = name
+
+    def __hash__(self):
+        return hash(self.name)
+
+    def __eq__(self, other):
+        return isinstance(other, State) and self.name == other.name
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return self.__str__()
+
 
 class Transition:
     def __init__(self):
@@ -6,24 +25,30 @@ class Transition:
         self.delta = {}
 
     def add(self, ei, ef, symbols):
-        self.states.add(ei)
-        self.states.add(ef)
+        assertion = [isinstance(s, str) for s in symbols]
+        assert sum(assertion) == len(assertion)
+
+        state_ei = State(ei)
+        state_ef = State(ef)
+
+        self.states.add(state_ei)
+        self.states.add(state_ef)
         self.alphabet.update(symbols)
 
-        if ei not in self.delta:
-            self.delta[ei] = {}
+        if state_ei not in self.delta:
+            self.delta[state_ei] = {}
 
         for s in symbols:
-            if s not in self.delta[ei]:
-                self.delta[ei][s] = set()
+            if s not in self.delta[state_ei]:
+                self.delta[state_ei][s] = set()
 
-            self.delta[ei][s].add(ef)
+            self.delta[state_ei][s].add(state_ef)
 
     def __str__(self):
         string = "states = " + str(self.states) + " ; alphabet = " + str(self.alphabet) + "\n"
         for e in self.delta:
             for s in self.delta[e]:
-                string += e + " (" + str(s) + ") -> " + str(self.delta[e][s]) + "\n"
+                string += str(e) + " (" + str(s) + ") -> " + str(self.delta[e][s]) + "\n"
         return string[:-1]
 
     def __repr__(self):
@@ -32,14 +57,14 @@ class Transition:
 
 class FiniteAutomata:
     def __init__(self, transition, initial, final):
-        assert isinstance(transition.states, set)
-        assert isinstance(transition.alphabet, set)
-        assert isinstance(final, set) and final.issubset(transition.states)
-        assert not isinstance(initial, set) and initial in transition.states
-
         self.transition = transition
-        self.initial = initial
-        self.final = final
+        self.initial = State(initial)
+        self.final = {State(e) for e in final}
+
+        assert isinstance(self.transition.states, set)
+        assert isinstance(self.transition.alphabet, set)
+        assert isinstance(self.final, set) and self.final.issubset(self.transition.states)
+        assert not isinstance(self.initial, set) and self.initial in self.transition.states
 
     def __str__(self):
         string = str(self.transition)
@@ -48,3 +73,28 @@ class FiniteAutomata:
 
     def __repr__(self):
         return self.__str__()
+
+    def build_dot(self):
+        dot = Digraph()
+        dot.attr(rankdir='LR', size='8,5')
+        dot.node("hidden", style="invisible")
+
+        if self.initial in self.final:
+            dot.node(str(self.initial), root="true", shape="doublecircle")
+        else:
+            dot.node(str(self.initial), root="true")
+
+        dot.edge("hidden", str(self.initial))
+
+        for state in self.transition.states:
+            if state in self.final:
+                dot.node(str(state), shape="doublecircle")
+            else:
+                dot.node(str(state))
+
+        for ei in self.transition.delta:
+            for symbol in self.transition.delta[ei]:
+                for ef in self.transition.delta[ei][symbol]:
+                    dot.edge(str(ei), str(ef), label=str(symbol))
+
+        return dot
